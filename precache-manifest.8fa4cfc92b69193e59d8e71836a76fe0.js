2d539f99efb5d94a65c7dd36f4022ff4406214ea
eg0000r <47872540+eg0000r@users.noreply.github.com>
2020-03-08 01:48:33 -0500

    Updates

---
+++ precache-manifest.8fa4cfc92b69193e59d8e71836a76fe0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f47498248e8566e3ff70370f51c62dd",
+    "url": "/roll/index.html"
+  },
+  {
+    "revision": "f4e19e5a31b7fe7ccd8c",
+    "url": "/roll/static/css/main.434c16bd.chunk.css"
+  },
+  {
+    "revision": "c2d739df8efa32a03884",
+    "url": "/roll/static/js/2.078f305b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/roll/static/js/2.078f305b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4e19e5a31b7fe7ccd8c",
+    "url": "/roll/static/js/main.e60bda73.chunk.js"
+  },
+  {
+    "revision": "996e5bb5773de97f3730",
+    "url": "/roll/static/js/runtime-main.e19f22a2.js"
+  }
+]);
